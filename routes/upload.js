1acfd3ddac172db3cdc3551e38dcd362da1c09de
bingo233 <1271162887@qq.com>
2018-11-30 00:43:49 +0800

    编写svg组件

--- routes/upload.js
+++ routes/upload.js
@@ -6,17 +6,20 @@ const fs = require('fs')
 const upload = multer({ dest: 'upload/' });
 
 router.post('/uploadSvg',upload.single('file'), (req, res) => {
-  console.log(req.file)
   let mres = []
   let readData = fs.readFileSync(req.file.path);
   readData = readData.toString()
-  mres = readData.match(/<PATH[^>/]*>(.*?)<\/PATH>/gi);
+  // mres = readData.match(/<PATH[^>/]*>(.*?)<\/PATH>/gi);
+  mres = readData.match(/(<PATH[^>]*>(.*?)\/>)|(<PATH[^>/]*>(.*?)<\/PATH>)/gi);
+  if (!mres || mres.length === 0) {
+    return res.send(msg.err('请上传正确的svg文件',null,readData))
+  }
   fs.writeFileSync(req.file.path,mres.join(''));
   res.send(mres)
 });
 
-router.get('/getSvg', (req,res) => {
-  let readData = fs.readFileSync('upload\\686b6cebe9504c2cd28686124b625493');
+router.get('/getSvg', ({query:{id}},res) => {
+  let readData = fs.readFileSync('upload\\'+id);
   res.send(readData)
 })
 
