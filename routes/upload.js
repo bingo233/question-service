9c1bd97dfbe2ebe78b79c8fbe41a65b06984dfe1
bingo233 <1271162887@qq.com>
2018-11-30 17:59:59 +0800

    svg下载

--- routes/upload.js
+++ routes/upload.js
@@ -10,12 +10,20 @@ router.post('/uploadSvg',upload.single('file'), (req, res) => {
   let readData = fs.readFileSync(req.file.path);
   readData = readData.toString()
   // mres = readData.match(/<PATH[^>/]*>(.*?)<\/PATH>/gi);
-  mres = readData.match(/(<PATH[^>]*>(.*?)\/>)|(<PATH[^>/]*>(.*?)<\/PATH>)/gi);
+  // mres = readData.match(/(<PATH[^>]*>(.*?)\/>)|(<PATH[^>/]*>(.*?)<\/PATH>)/gi);
+  // 取svg标签
+  mres = readData.match(/<SVG[^>].*?<\/SVG>/i);
+  
   if (!mres || mres.length === 0) {
     return res.send(msg.err('请上传正确的svg文件',null,readData))
   }
-  fs.writeFileSync(req.file.path,mres.join(''));
-  res.send(mres)
+  // 替换fill属性为 currentColor
+  let result = mres.join('').replace(/fill *= *(\"|\').+?(\"|\')/gi,' fill="currentColor"')
+  // 替换width 和 height 属性为 100%
+  result = result.replace(/width *= *(\"|\').+?(\"|\')/gi,' width="100%"').replace(/height *= *(\"|\').+?(\"|\')/gi,' height="100%"')
+  fs.writeFileSync(req.file.path,result);
+  // res.send(msg.ok('文件上传成功',null,req.file.filename))
+  res.send(req.file)
 });
 
 router.get('/getSvg', ({query:{id}},res) => {
