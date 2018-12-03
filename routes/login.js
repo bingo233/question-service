417f8b7dd1951530a571cada23f25a52a827f2c1
bingo233 <1271162887@qq.com>
2018-12-03 14:20:28 +0800

    修改登录拦截器

--- routes/login.js
+++ routes/login.js
@@ -3,15 +3,44 @@ const router = express.Router();
 const userSql = require('../query/user');
 const msg = require('../utils/msg');
 
-router.post('/loginCheck', ({query: {acc, pwd}, session}, res) => {
+// router.post('/loginCheck', ({query: {acc, pwd}, session}, res) => {
+// 	// res.json("req")
+// 	userSql.checkUser([acc, pwd],(user) => {
+// 		if(user){
+// 			console.log(user,'22222222222222222222222222')
+// 			return session.regenerate(err => {
+// 				if(err) {
+// 					return res.json(msg.creatMsg('登录失败',555,'请重新尝试',false))
+// 				}
+// 				delete user.pwd;
+// 				session.user = user;
+// 				return res.json(msg.ok('登录成功',null,session.id));
+// 			})
+// 		}
+// 		return res.json(msg.creatMsg('登录失败',555,'用户名或密码错误！',false));
+// 	});
+  
+// });
+
+router.post('/loginCheck', (req, res) => {
 	// res.json("req")
+	let acc = req.query.acc;
+	let pwd = req.query.pwd;
+
 	userSql.checkUser([acc, pwd],(user) => {
 		if(user){
+			req.session.regenerate(function(err){
+			if(err) {
+				return res.json(msg.creatMsg('登录失败',555,'请重新尝试',false))
+			}
 			delete user.pwd;
-			session.user = user;
-			return res.json(msg.ok('登录成功',null,session.id));
+			req.session.user = user;
+			return res.json(msg.ok('登录成功',null,req.session.id));
+			})
+		}else {
+			return res.json(msg.creatMsg('登录失败',555,'用户名或密码错误！',false));
 		}
-		return res.json(msg.creatMsg('登录失败',555,'用户名或密码错误！',false));
+		
 	});
   
 });
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+var FileStore = require('session-file-store')(session);
 const msg = require('./utils/msg');
 
 const app = express();
+
+// =====session===== //
+app.use(session({
+	// name: 'em_question',
+	secret: 'chyingp',  // 用来对session id相关的cookie进行签名
+	store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+	saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
+	resave: false,//,  // 是否每次都重新保存会话，建议false
+	// httpOnly: false,
+	cookie: {
+		maxAge: 3600 * 1000  // 有效期，单位是毫秒
+	}
+}));
+// app.use(express.favicon());
 
 // =====路由===== //
 const routers = require('./routes/index');
@@ -21,25 +36,12 @@ app.all('*', function(req, res, next) {
 	next();
 });
 
-// =====session===== //
-app.use(session({
-	name: 'em_question',
-	secret: 'chyingp',  // 用来对session id相关的cookie进行签名
-	//store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
-	saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
-	resave: false//,  // 是否每次都重新保存会话，建议false
-	// cookie: {
-	// 	maxAge: 10 * 1000  // 有效期，单位是毫秒
-	// }
-}));
-
 // =====登录拦截====== //
 //登录拦截器，必须放在静态资源声明之后、路由导航之前
 app.use(function (req, res, next) {
 	var url = req.originalUrl;
 	url = url.split('?')[0];
-    debugger
-	console.log(req.session);
+
 	if (url != '/login/loginCheck' && !req.session.user) {
 		return res.json(msg.logonFailure());
 	}
